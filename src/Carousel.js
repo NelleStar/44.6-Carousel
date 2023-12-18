5e8af0276d6c83ca317c4ae5fa5a074eda162381
Janelle <janelle.k.houston@gmail.com>
2023-12-18 15:55:03 -0500

    updating files from Part 2 of the homework

--- src/Carousel.js
+++ src/Carousel.js
@@ -2,52 +2,40 @@ import { useState } from "react";
 import "./Carousel.css";
 import Card from "./Card";
 
-
 /** Carousel: displays images and arrows to navigate through them
- * 
+ *
  * Props:
  * - photos: array of {src, caption} objects
  * - title: string describing the collection of images
- * 
+ *
  * State:
  * - currCardIdx: integer for current card index
- * 
+ *
  * App --> Carousel --> Card
  */
- function Carousel({ photos, title }) {
+function Carousel({ photos, title }) {
   const [currCardIdx, setCurrCardIdx] = useState(0);
 
   const currCard = photos[currCardIdx];
   const total = photos.length;
 
+  //Increments currCardIdx state by 1
   function goForward() {
-    // Increment currCardIdx state by 1 (move forward)
-    setCurrCardIdx((prevIdx) => (prevIdx + 1) % total);
-  }
-
-  function goBackward() {
-    // Decrement currCardIdx state by 1 (move backward)
-    setCurrCardIdx((prevIdx) => (prevIdx - 1 + total) % total);
+    setCurrCardIdx(currCardIdx + 1);
   }
 
   return (
     <div className="Carousel">
       <h1>{title}</h1>
       <div className="Carousel-main">
-        <i
-          className="bi bi-arrow-left-circle"
-          onClick={goBackward}
-        />
+        <i className="bi bi-arrow-left-circle" onClick={goForward} />
         <Card
           caption={currCard.caption}
           src={currCard.src}
           currNum={currCardIdx + 1}
           totalNum={total}
         />
-        <i
-          className="bi bi-arrow-right-circle"
-          onClick={goForward}
-        />
+        <i className="bi bi-arrow-right-circle" onClick={goForward} />
       </div>
     </div>
   );
